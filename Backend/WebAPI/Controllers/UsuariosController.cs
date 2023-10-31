4977311fa6f3c770961ee4435907a27565dac73c
Pablo Ferreira <39393083+PabloVizard@users.noreply.github.com>
2023-10-31 02:57:33 -0300

    Adicionado usuario controller

---
+++ Backend/WebAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Applications.Interfaces;
+using Application.Models;
+using Entities.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsuariosController : BaseController<Usuarios, UsuariosModel>
+    {
+        private readonly IUsuariosApp _usuariosApp;
+        public UsuariosController(IUsuariosApp usuariosApp) : base(usuariosApp)
+        {
+            _usuariosApp = usuariosApp;
+        }
+    }
+}
